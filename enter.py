05bb309aa5a5925a3b7f23fe287b14c180ed404d
devansh-dvj <devansh.dvj@gmail.com>
2020-06-09 02:06:34 +0530

    Added MIT 6.042J notes

---
+++ enter.py
@@ -0,0 +1,11 @@
+file = open('MIT-6.042J.tex', 'w')
+
+for i in range(23, 78):
+	file.write('\\newpage\n\\begin{figure}\n    \\centering\n    \\includegraphics[scale=0.25]{"./MIT 6.042J/MIT_6042J_')
+	if i in range(1, 10):
+		file.write('00' + str(i))
+	if i in range(10, 100):
+		file.write('0' + str(i))
+	if i in range(100, 1000):
+		file.write(str(i))
+	file.write('"}\n\\end{figure}\n')
